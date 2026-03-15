9b6a81d347233cca17d4c851560c8ec96cfa1a27
Sg <123773436+sgspec@users.noreply.github.com>
2026-03-16 03:11:44 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@ export default {
     const dream = url.searchParams.get("dream") || "ฝันเห็นงู";
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=" + env.GEMINI_KEY,
+      "https://generativelanguage.googleapis.com/v1beta/models/Gemini-3.1-Flash-Lite-preview:generateContent?key=" + env.GEMINI_KEY,
       {
         method: "POST",
         headers: {
